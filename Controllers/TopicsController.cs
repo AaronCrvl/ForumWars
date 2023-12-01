b560f7c542ef29da0d9cc20aa9018839eb0d7733
Aaron <72924198+AaronCrvl@users.noreply.github.com>
2023-11-30 21:12:36 -0300

    📟new page

--- Controllers/TopicsController.cs
+++ Controllers/TopicsController.cs
@@ -5,58 +5,71 @@ namespace ForumWars.Controllers
 {
     public class TopicsController : Controller
     {
+        #region Propeties
         private Random gen = new Random();
+        private List<Topic> _topics = new List<Topic>
+        {
+            new Topic
+            {
+                Id = 1,
+                Title = "Why start with NextJS ?",
+                UpVotes = 84,
+                TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                CommunityTopics = new List<string>{ "Development", "React", "Next"}
+            },
+            new Topic
+            {
+                Id = 2,
+                Title = "Invincible Season 2",
+                TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                UpVotes = 45,
+                CommunityTopics = new List<string>{ "Streaming", "Netflix" }
+            },
+            new Topic
+            {
+                Id = 3,
+                Title = ".NET Blazor Server",
+                TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                CommunityTopics = new List<string>{ ".NET", ".NET Core" },
+                UpVotes = 32,
+            },
+            new Topic
+            {
+                Id = 4,
+                Title = "New Salmon Receipt!",
+                TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                CommunityTopics = new List<string>{ "Food", "Cooking", "Kitchen"},
+                UpVotes = 25,
+            },
+            new Topic
+            {
+                Id = 5,
+                Title = "Leet Code - A New Way To Prepare Yourself",
+                TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                CommunityTopics = new List<string>{ "Development", "LeetCode" },
+                UpVotes = 8,
+            }
+        };
+        #endregion
+
+        #region Constructors
         public TopicsController()
         {
 
         }
+        #endregion
 
+        #region Methods
         public Task<List<Topic>> GetTopicList()
         {
             Task.Delay(2000); // latência de 2 segundos
-            return Task.FromResult(new List<Topic>
-            {
-                new Topic
-                {
-                    Id = 1,
-                    Title = "Why start with NextJS ?",
-                    UpVotes = 84,
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    CommunityTopics = new List<string>{ "Development", "React", "Next"}
-                },
-                new Topic
-                {
-                    Id = 2,
-                    Title = "Invincible Season 2",
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    UpVotes = 45,
-                    CommunityTopics = new List<string>{ "Streaming", "Netflix" }
-                },
-                new Topic
-                {
-                    Id = 3,
-                    Title = ".NET Blazor Server",
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    CommunityTopics = new List<string>{ ".NET", ".NET Core" },
-                    UpVotes = 32,
-                },
-                new Topic
-                {
-                    Id = 4,
-                    Title = "New Salmon Receipt!",
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    CommunityTopics = new List<string>{ "Food", "Cooking", "Kitchen"},
-                    UpVotes = 25,
-                },
-                new Topic
-                {
-                    Id = 5,
-                    Title = "Leet Code - A New Way To Prepare Yourself",
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    CommunityTopics = new List<string>{ "Development", "LeetCode" },
-                    UpVotes = 8,
-                }
-            });
+            return Task.FromResult(_topics);
+        }
+
+        public Task<List<Topic>> SearchInTopicList(string typedText)
+        {
+            Task.Delay(1000); // latência de 2 segundos
+            return Task.FromResult(_topics.Where(x => x.Title.Contains(typedText) == true).ToList<Topic>());
         }
 
         public Task<Topic> GetTopic(int idTopic)
@@ -64,46 +77,11 @@ namespace ForumWars.Controllers
             Task.Delay(2000); // latência de 2 segundos
             return Task.FromResult(idTopic switch
             {
-                1 => new Topic
-                {
-                    Id = 1,
-                    Title = "Why start with NextJS ?",
-                    UpVotes = 84,
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    CommunityTopics = new List<string> { "Development", "React", "Next" }
-                },
-                2 => new Topic
-                {
-                    Id = 2,
-                    Title = "Invincible Season 2",
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    UpVotes = 45,
-                    CommunityTopics = new List<string> { "Streaming", "Netflix" }
-                },
-                3 => new Topic
-                {
-                    Id = 3,
-                    Title = ".NET Blazor Server",
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    CommunityTopics = new List<string> { ".NET", ".NET Core" },
-                    UpVotes = 32,
-                },
-                4 => new Topic
-                {
-                    Id = 4,
-                    Title = "New Salmon Receipt!",
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    CommunityTopics = new List<string> { "Food", "Cooking", "Kitchen" },
-                    UpVotes = 25,
-                },
-                5 => new Topic
-                {
-                    Id = 5,
-                    Title = "Leet Code - A New Way To Prepare Yourself",
-                    TopicText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    CommunityTopics = new List<string> { "Development", "LeetCode" },
-                    UpVotes = 8,
-                },
+                1 => _topics[1],
+                2 => _topics[2],
+                3 => _topics[3],
+                4 => _topics[4],
+                5 => _topics[5],
                 _ => new Topic
                 {
                     Title = "The default topic",
@@ -169,5 +147,6 @@ namespace ForumWars.Controllers
             int range = (DateTime.Today - start).Days;
             return start.AddDays(gen.Next(range));
         }
+        #endregion              
     }
 }
